b9888667c3a4886f64cd330df7c921a0cec70d29
brxck999 <pashokbagin@gmail.com>
2025-05-13 08:39:05 +0300

    fix

--- bot.py
+++ bot.py
@@ -4,25 +4,24 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
-def greet_user(update, context):
+async def greet_user(update, context):
     print("Вызван /start")
-    update.message.reply_text("Здравствуй, пользователь")
+    await update.message.reply_text("Здравствуй, пользователь")
 
-def talk_to_me(update, context):
+async def talk_to_me(update, context):
     text = update.message.text
     print(text)
-    update.message.reply_text(text)
+    await update.message.reply_text(text)
 
 def main():
-    mybot = Application.builder().token(settigs.API_KEY).build()
+    mybot = Application.builder().token(settings.API_KEY).build()
 
     mybot.add_handler(CommandHandler("start", greet_user))
     mybot.add_handler(MessageHandler(filters.TEXT, talk_to_me))
 
-    logging.INFO("Бот стартовал")
+    logging.info("Бот стартовал")  # Используем logging.info() вместо logging.INFO()
 
     mybot.run_polling()
-
 
 if __name__ == '__main__':
     main()
